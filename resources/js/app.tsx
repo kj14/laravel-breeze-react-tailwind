b8306f81819b61fdf8da8a2ebf036585d995f768
Koji Tanemura <tnmrkj+github@gmail.com>
2022-12-14 11:55:08 +0900

    laravel-breeze-react-tailwindの初期設定

---
+++ resources/js/app.tsx
@@ -0,0 +1,29 @@
+import "./bootstrap";
+import "../css/app.css";
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        ),
+    setup({ el, App, props }) {
+        const container = document.getElementById('app');
+        if (container) {
+            const root = createRoot(container);
+            root.render(<App {...props} />);
+        }
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
